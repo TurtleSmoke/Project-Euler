5a26f1a544ab8cb6307159f80a0f26852558ccc3
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2021-09-05 20:13:34 +0200

    feat(p12): solution to problem 0012

---
+++ problems/problem_0012/solution1.py
@@ -0,0 +1,20 @@
+def number_of_divisors(n):
+    res = 1
+    for i in range(2, n):
+        if n % i == 0:
+            res += 1
+    return res
+
+
+def highly_div_triangular_number(n=500):
+    i = 1
+    res = 1
+    while number_of_divisors(res) < n:
+        i += 1
+        res += i
+
+    return res
+
+
+if __name__ == "__main__":
+    print(highly_div_triangular_number())

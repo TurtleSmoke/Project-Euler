2c8da84dc94ec256b5f3a8695706aa10d011e4af
TurtleSmoke <46165291+TurtleSmoke@users.noreply.github.com>
2021-09-15 23:02:58 +0200

    feat(p17): solution to problem 0017

---
+++ problems/problem_0017/solution1.py
@@ -0,0 +1,12 @@
+def number_letter_counts():
+    unit = len("onetwothreefourfivesixseveneightnine")
+    ten = len("teneleventwelvethirfourfifsixseveneighnine") + len("teen") * 7
+    and_l = len("and")
+    twenty = len("twentythirtyfortyfiftysixtyseventyeightyninety")
+    hun = len("hundred")
+    thou = len("onethousand")
+    return thou + 900 * hun + 190 * unit + 100 * twenty + 891 * and_l + 10 * ten
+
+
+if __name__ == "__main__":
+    print(number_letter_counts())

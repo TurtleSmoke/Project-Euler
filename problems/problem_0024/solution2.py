1f2a88b398653701be15054c4c1ce4046f5acbde
TurtleSmoke <46165291+TuWG@users.noreply.github.com>
2021-10-01 01:16:33 +0200

    feat(p24): solution to problem 0024

---
+++ problems/problem_0024/solution2.py
@@ -0,0 +1,12 @@
+from math import factorial
+
+
+def lexicographic_permutations(s, n):
+    if len(s) <= 1:
+        return s
+    q, r = divmod(n, factorial(len(s) - 1))
+    return s[q] + lexicographic_permutations(s[:q] + s[q + 1:], r)
+
+
+if __name__ == "__main__":
+    print(lexicographic_permutations("0123456789", 999999))

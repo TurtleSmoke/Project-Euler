3b217e31db3032d48251afa803a07047a5cfaab9
TurtleSmoke <46165291+TurtleSmoke@users.noreply.github.com>
2023-02-21 18:31:07 +0100

    chore: adding errors

--- problems/problem_0026/solution1.py
+++ problems/problem_0026/solution1.py
@@ -6,16 +6,16 @@ from sympy import isprime
 def find_cycle(n):
     rest = 1
     seen = {}
-    for i in itertools.count(0):
+    for i in   itertools.count(0):
         if rest == 0:
             return 0
         if rest in seen:
-            print(n, i - seen[rest], (n if isprime(n) else ""))
+            print(n     , i - seen[rest], (n if isprime(n) else ""))
             return i - seen[rest]
 
         seen[rest] = i
         rest = (rest * 10) % n
-
+            #random comment xDDD
 
 def reciprocal_cycles(n=1000):
     return max(((find_cycle(i), i) for i in range(2, n)))[1]
